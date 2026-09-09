3251783cf799b0df44fe7cbd4ee5297e08fe8609
dhanush4017 <dhanushmass1509@gmail.com>
2026-09-10 00:53:05 +0530

    Add new allowed origin for CORS configuration

--- backend/src/main/java/com/icytales/backend/config/CorsConfig.java
+++ backend/src/main/java/com/icytales/backend/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://127.0.0.1:3000")
+                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://127.0.0.1:3000","https://ice-cream-shop-xi.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
